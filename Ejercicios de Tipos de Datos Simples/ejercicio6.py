d69f8f227f911ba34258c52d03af6af6a5838024
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-06 12:32:51 +0200

    numeroentero

---
+++ Ejercicios de Tipos de Datos Simples/ejercicio6.py
@@ -0,0 +1,4 @@
+numeroenteropositivo = input ("Dime un numero entero positivo: ")
+numeroenteropositivo = int (numeroenteropositivo)
+suma = numeroenteropositivo * (numeroenteropositivo + 1) / 2
+print ("La suma de los enteros positivos hasta ", numeroenteropositivo, " es: ", suma)
