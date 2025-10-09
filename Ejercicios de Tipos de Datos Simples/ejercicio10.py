cd4ec6e98f3bb45f2cff9e2b274e2a2c756d60cb
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-09 09:56:08 +0200

    pedidopayasos

--- Ejercicios de Tipos de Datos Simples/ejercicio10.py
+++ Ejercicios de Tipos de Datos Simples/ejercicio10.py
@@ -1,6 +1,6 @@
-numerodepayasos = input("Dime el numero de payasos")
-numerodemuñecas = input("Dime el numero de muñecas")
+numerodepayasos = input("Dime el numero de payasos ")
+numerodemuñecas = input("Dime el numero de muñecas ")
 pesopayaso = 112
 pesomuñeca = 75
 pesototal = (int (numerodepayasos) * int (pesopayaso)) + (int(numerodemuñecas) * int (pesomuñeca))
-print (pesototal)   
+print ("Son",pesototal,"gramos")  
