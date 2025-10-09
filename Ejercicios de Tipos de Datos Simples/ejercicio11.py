a61e0ac15dc4f53cc4c1b9662ce90f7b63d57693
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-09 11:06:40 +0200

    12 pan

---
+++ Ejercicios de Tipos de Datos Simples/ejercicio11.py
@@ -0,0 +1,3 @@
+cantidaddinerodepositado = input("Dime la cantidad de dinero")
+dineroahorrado = float(cantidaddinerodepositado) * 1.04
+print ("El dinero ahorrado es: ", dineroahorrado)
---
+++ Ejercicios de Tipos de Datos Simples/ejercicio12.py
@@ -0,0 +1,7 @@
+preciopan = 3.49
+preciocondescuento = preciopan * 0.4
+barrasvendidas = input("Dime cuantas barras de pan se han vendido hoy: ")
+total = float (barrasvendidas) * float (preciocondescuento)
+print ("El precio del pan sin descuento es: ", (preciopan))
+print ("El precio del pan con descuento es: ", preciocondescuento)
+print ("El total de la venta es: ", total)
