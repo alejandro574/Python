1dee56c7b25428ab30dd0ebcadb12dc8b55626e5
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-23 11:01:11 +0200

    division 3 if

---
+++ ejercicios condicionales/3.py
@@ -0,0 +1,9 @@
+numero1 = input("dime n1 ")
+numero2 = input("dime n2 ")
+if numero2 == "0":
+    print("Error, no se puede dividir por 0")
+else:
+    cociente = int(numero1) / int(numero2)
+    print("El cociente es: ", cociente)
+
+
