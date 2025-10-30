db18fe36fd7c5bc9837eb21b85fe2ed6bfb8a46a
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-30 10:11:20 +0100

    Ejercicio10Condicional

--- ejercicios condicionales/10.py
+++ ejercicios condicionales/10.py
@@ -1,7 +1,18 @@
 pizza = str(input("Quieres una pizza vegetariana"))
 if pizza == "si":
     print ("Has elegido la pizza vegetariana")
-    print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Pimiento y Tofu")
+    elige1 = input ("Elige 1 de estos ingredientes para tu pizza: Pimiento y Tofu ")
+    if elige1 == "Pimiento":
+         print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,")
+    else:
+            print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Tofu")
 else:
     print ("Has elegido la pizza NO  vegetariana")
-    print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Peperoni,Jamón y Salmón")
+    elige2 = input ("Elige 1 de estos ingredientes para tu pizza: Peperoni, Jamón y Salmón ")
+    if elige2 == "Peperoni":
+         print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Peperoni")
+    elif elige2 == "Jamón":
+            print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Jamón")
+    else:
+            print ("Tiene los siguientes ingredientes","Mozzarela,Tomate,Salmón")
+
