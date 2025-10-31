13e48befe26ef54d18057f9d28adea8a8b11e148
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-10-31 14:38:46 +0100

    ejercicio7bucles

---
+++ Ejercicios Bucles/7.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+for i in range(1,11):
+    print("Tabla del ", i)
+    for j in range(1,11):
+        print(i, " x ", j, " = ", i*j)
