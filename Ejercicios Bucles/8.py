b275233b4ee59e34ae87f2b75e4d32d62b40783d
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-11-06 09:49:01 +0100

    Ejercicio8Bucles

---
+++ Ejercicios Bucles/8.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo.
+numeroentero = int(input("Introduce un número entero positivo: "))
+for i in range(1, numeroentero + 1):
+    print(' ' * (numeroentero - i) + '*' * i)
