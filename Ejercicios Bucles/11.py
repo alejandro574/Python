0a4281669f7e8925b1ec0b1491113f86497ed3ca
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-11-07 14:39:37 +0100

    Ejercicio11Bucles

--- Ejercicios Bucles/11.py
+++ Ejercicios Bucles/11.py
@@ -1,6 +1,4 @@
 palabra = input("Introduce una palabra para darle la vuelta: ")
-palabra_invertida = ""
-for letra in palabra:
-    palabra_invertida = letra + palabra_invertida
-print("La palabra invertida es:", palabra_invertida)
+for palabra in reversed(palabra):
+    print(palabra)
 
