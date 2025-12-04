edefd98b0eee0de738043731fe58eb9e16fcb2e9
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-12-04 10:25:39 +0100

    Ejercicio4Listas

---
+++ Ejercicios LISTAS/4.py
@@ -0,0 +1,7 @@
+numeros = []
+for i in range(0,6):
+    numero= int(input("Introduce un número ganador: "))
+    numeros.append(numero)
+numeros.sort()
+print("Los números ganadores ordenados son:", numeros)
+
