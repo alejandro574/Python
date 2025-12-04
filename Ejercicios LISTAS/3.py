3f05977da80d8fa4caacf27be4b245d79cb3aa4b
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2025-12-04 09:56:31 +0100

    Ejercicios3Listas

---
+++ Ejercicios LISTAS/3.py
@@ -0,0 +1,4 @@
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+for asignatura in asignaturas:
+    nota = input("Nota de " + asignatura + ": ")
+    print("En", asignatura, "has sacado", nota)
