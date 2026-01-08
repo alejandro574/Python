0bd030dac053b8a5c3ae19db7255b0e06eeb185e
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2026-01-08 09:27:38 +0100

    eliminar

--- Ejercicios LISTAS/6.py
+++
@@ -1,7 +0,0 @@
-asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
-suspensas = []   
-for asignatura in asignaturas:
-    nota = float(input("Nota de " + asignatura + ": "))
-    if nota < 5:              
-        suspensas.append(asignatura)
-print("Tienes que repetir:", suspensas)
--- Ejercicios LISTAS/7.py
+++
@@ -1,8 +0,0 @@
-abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
-resultado = []
-posicion = 1
-for letra in abecedario:
-    if posicion % 3 != 0:  
-        resultado.append(letra)
-    posicion+=1
-print(resultado)
