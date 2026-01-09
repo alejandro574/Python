1f080d12fa8cc4f458747be5a1c06870047f0130
Alejandro Martin Fernandez <amarfer574@ieszaidinvergeles.org>
2026-01-09 14:22:20 +0100

    ejercicio2Diccionarios

--- Ejercicios Diccionarios/2.py
+++ Ejercicios Diccionarios/2.py
@@ -1,8 +1,11 @@
-
-
+nombre = input("Dime tu nombre")
+edad = input("Dime tu edad")
+direccion = input("Dime tu direccion")
+telefono = input("Dime tu telefono")
 usuario = {
-    'nombre': '€',
-    'edad': '$',
-    'dirección': '¥',
-    'teléfono' : 'f'
+    'nombre': nombre,
+    'edad': edad,
+    'dirección': direccion,
+    'teléfono' : telefono
 }
+print (usuario['nombre']," tiene", usuario['edad'] ," años, vive en", usuario['dirección']," y su número de teléfono es", usuario['teléfono'])
